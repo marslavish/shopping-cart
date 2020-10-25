2c8f616a0ffef06efd211b51e5fee088d0493289
marslavish <uniqueye608@gmail.com>
2020-10-25 19:57:41 +0800

    Updates

---
+++ precache-manifest.b0442187a14c7744ce20828be1588996.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ccd2d9c98c02d28c121a5db0a8373b9",
+    "url": "/shopping-cart/index.html"
+  },
+  {
+    "revision": "8e4694dc434ec32c791a",
+    "url": "/shopping-cart/static/css/main.fc1344ec.chunk.css"
+  },
+  {
+    "revision": "418e305ef717ecb180a7",
+    "url": "/shopping-cart/static/js/2.38614080.chunk.js"
+  },
+  {
+    "revision": "8d190e2d02f8e768921a63f56a741895",
+    "url": "/shopping-cart/static/js/2.38614080.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e4694dc434ec32c791a",
+    "url": "/shopping-cart/static/js/main.86e78835.chunk.js"
+  },
+  {
+    "revision": "2fd2112b4c9d1aff7220",
+    "url": "/shopping-cart/static/js/runtime-main.9a16983b.js"
+  }
+]);
